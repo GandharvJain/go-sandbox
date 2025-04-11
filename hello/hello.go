261ba9cb347ca824b43c3edada9a6f725327e35e
Gandharv Jain <78318488+GandharvJain@users.noreply.github.com>
2025-04-12 01:38:49 +0530

    Add error handling for missing name in Hello()

--- hello/hello.go
+++ hello/hello.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
-import "rsc.io/quote"
+import (
+	"fmt"
+	"log"
+	"example.com/greetings"
+)
 
 func main() {
-	fmt.Println(quote.Go())
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and a line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Get a greeting message and print it.
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
 }
